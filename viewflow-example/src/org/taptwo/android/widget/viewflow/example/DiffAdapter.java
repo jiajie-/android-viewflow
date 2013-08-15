e52c405c6c688ad4a1c823ffc001a7a409dfe69e
jiajie- <654350754@qq.com>
2013-08-15 17:40:12 +0800

    test change

--- viewflow-example/src/org/taptwo/android/widget/viewflow/example/DiffAdapter.java
+++ viewflow-example/src/org/taptwo/android/widget/viewflow/example/DiffAdapter.java
@@ -15,7 +15,7 @@ public class DiffAdapter extends BaseAdapter implements TitleProvider {
         private static final int VIEW_MAX_COUNT = VIEW2 + 1;
     	private final String[] names = {"View1","View2"};
 
-    private LayoutInflater mInflater;
+    private LayoutInflater mInflater;//test change
 
     public DiffAdapter(Context context) {
         mInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
